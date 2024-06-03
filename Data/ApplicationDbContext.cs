fc9b26823176c937df94df7a7dea342f0a293a44
Mohamad Hicham Tarif <mohamad.tarif@levva.io>
2024-06-02 22:16:54 -0300

    Initial Commit Template

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Template_Desafio_Ods_Comunidades.Models;
+
+
+namespace Template_Desafio_Ods_Comunidades.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+              : base(options)
+        {
+        }
+
+        public DbSet<Oligarquia> Oligarquia {get;set;}
+    }
+
+
+}
---
+++ DockerCompose/CreateDataBase.sql
@@ -0,0 +1,12 @@
+﻿create database Desafio_Ods_Comunidades
+
+CREATE TABLE oligarquia (
+    id_oligarquia SERIAL PRIMARY KEY,
+    tipo_base VARCHAR(255),
+    sigla VARCHAR(255),
+    nome VARCHAR(255),
+    quantidade_bases INT,
+    quantidade_registros INT,
+    concluido BOOLEAN,
+    interessante BOOLEAN
+);
