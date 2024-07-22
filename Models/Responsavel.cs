b3376fce22daf9cf92ad275ca1da4260429dd2af
Mohamad Hicham Tarif <hamudetrf@gmail.com>
2024-07-22 16:58:39 -0300

    Update Responsavel.cs

--- Models/Responsavel.cs
+++ Models/Responsavel.cs
@@ -1,1 +1,22 @@
 
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Template_Desafio_Ods_Comunidades.Models
+{
+    public class Responsavel
+    {
+        [Key]
+        public string Email { get; set; }
+        
+
+        [Required(ErrorMessage = "O Nome do Responsável é obrigatório.")]
+        public string Nome {get; set;}
+
+        [Required(ErrorMessage = "O Celular do Responsável é obrigatório.")]
+        public string Celular {get; set;}
+
+       [ForeignKey("Secretaria")]
+        public string SiglaSecretaria { get; set; }
+    }
+}
