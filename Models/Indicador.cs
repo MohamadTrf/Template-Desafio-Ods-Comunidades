391002e35fd20557b8c3234bada5475b5db1d6d5
Mohamad Hicham Tarif <hamudetrf@gmail.com>
2024-07-22 17:10:33 -0300

    Update and rename Indicador to Indicador.cs

---
+++ Models/Indicador.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Template_Desafio_Ods_Comunidades.Models
+{
+    public class Indicador
+    {
+        public int IdCodigoArquivo { get; set; }
+        public int IdCodigoValor { get; set; }
+    
+        public double ValorIndicador { get; set; }
+        public double Mediana { get; set; }
+        public double DesvioPadrao { get; set; }
+        public double LimiteSuperior { get; set; }
+        public double LimiteInferior { get; set; }
+        public int IdOds { get; set; }
+        public string DescricaoOds { get; set; }
+        
+        [ForeignKey("Responsavel")]
+        public string Email { get; set; }
+        
+        [ForeignKey("Secretaria")]
+        public string SiglaSecretaria { get; set; }
+
+
+        public virtual Responsavel Responsavel { get; set; }
+        public virtual Secretaria Secretaria { get; set; }
+    }
+}
