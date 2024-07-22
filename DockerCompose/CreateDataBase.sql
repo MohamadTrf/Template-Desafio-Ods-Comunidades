57b9fce8ce6aefbc238bb375e96b2475983abfb4
Mohamad Hicham Tarif <hamudetrf@gmail.com>
2024-07-22 16:51:35 -0300

    Update CreateDataBase.sql

--- DockerCompose/CreateDataBase.sql
+++ DockerCompose/CreateDataBase.sql
@@ -1,30 +1,30 @@
 create database Desafio_Ods_Comunidades
 
 
-CREATE TABLE "Fonte" (
-	"SiglaFonte" VARCHAR(20) PRIMARY KEY,
-	"NomeFonte" VARCHAR (45) UNIQUE NOT NULL
+CREATE TABLE "Secretaria" (
+    "SiglaSecretaria" VARCHAR(20) PRIMARY KEY,
+    "NomeSecretaria" VARCHAR(45) UNIQUE NOT NULL
 );
 
-CREATE TABLE "Contato_Fonte" (
-	"EmailContato" VARCHAR(50) PRIMARY KEY,
-	"NomeContato" VARCHAR(45) UNIQUE NOT NULL,
-	"CelularContato"VARCHAR(45) UNIQUE NOT NULL,
-	"SiglaFonte" VARCHAR(20) NOT NULL REFERENCES "Fonte"("SiglaFonte") ON DELETE CASCADE
+CREATE TABLE "Responsavel" (
+    "Email" VARCHAR(50) PRIMARY KEY,
+    "Nome" VARCHAR(45) NOT NULL,
+    "Celular" VARCHAR(45) UNIQUE NOT NULL,
+    "SiglaSecretaria" VARCHAR(20) NOT NULL REFERENCES "Secretaria"("SiglaSecretaria") ON DELETE CASCADE
 );
 
-
-CREATE TABLE "Ods" (
-	"IdOds" SERIAL PRIMARY KEY,
-	"DescricaoOds" VARCHAR(45) 
+CREATE TABLE "Indicadores" (
+    "IdCodigoArquivo" FLOAT,
+    "IdCodigoValor" FLOAT,
+    "ValorIndicador" DOUBLE PRECISION,
+    "Mediana" DOUBLE PRECISION,
+    "DesvioPadrao" DOUBLE PRECISION,
+    "LimiteSuperior" DOUBLE PRECISION,
+    "LimiteInferior" DOUBLE PRECISION,
+    "IdOds" INT,
+    "DescricaoOds" TEXT,
+    "Email" VARCHAR(50) NOT NULL REFERENCES "Responsavel"("Email") ON DELETE CASCADE,
+    "SiglaSecretaria" VARCHAR(20) NOT NULL REFERENCES "Secretaria"("SiglaSecretaria") ON DELETE CASCADE,
+    PRIMARY KEY ("IdCodigoArquivo", "IdCodigoValor")
 );
 
-
-CREATE TABLE "Outlier" (
-	"IdValorDigitado"  SERIAL PRIMARY KEY,
-	"DataValorDigitado" DATE,
-	"ValorDadoDigitado" INT,
-	"UrlColetaValor" VARCHAR(45),
-	"SiglaFonte" VARCHAR(20) NOT NULL REFERENCES "Fonte"("SiglaFonte") ON DELETE CASCADE
-	"IdOds" INT NOT NULL REFERENCES "Ods"("IdOds") ON DELETE CASCADE
-);
