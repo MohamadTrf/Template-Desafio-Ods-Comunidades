c53418fa2b955faa362cad6d6759b619ef1c37d2
Mohamad Hicham Tarif <hamudetrf@gmail.com>
2024-07-22 16:54:53 -0300

    Update Secretaria.cs

--- Models/Secretaria.cs
+++ Models/Secretaria.cs
@@ -1,1 +1,17 @@
 
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Template_Desafio_Ods_Comunidades.Models
+{
+    public class Secretaria
+    {
+        [Key]
+        public string SiglaSecretaria { get; set; }
+        
+
+        [Required(ErrorMessage = "O Nome da Secretaria é obrigatório.")]
+        public string NomeSecretaria {get; set;}
+        
+    }
+}
