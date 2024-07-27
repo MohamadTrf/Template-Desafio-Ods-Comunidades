11315650a080d7cbbce43929c0f5f972f934056d
Rafael Chineli <Rafael.chineli@gmail.com>
2024-07-27 09:49:24 -0300

    Secretaria

--- Models/Secretaria.cs
+++ Models/Secretaria.cs
@@ -7,11 +7,10 @@ namespace Template_Desafio_Ods_Comunidades.Models
     public class Secretaria
     {
         [Key]
-        public string SiglaSecretaria { get; set; }
-        
-
+        public string SiglaSecretaria { get; set; }        
         [Required(ErrorMessage = "O Nome da Secretaria é obrigatório.")]
         public string NomeSecretaria {get; set;}
+        public bool Active { get; set; }
         
     }
 }
